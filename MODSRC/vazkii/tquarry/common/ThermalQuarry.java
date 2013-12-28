05223ca31b114f6e652ec44f7344fb63cce8fb60
Vasco Lavos <vasco.men@hotmail.com>
2013-12-28 18:32:45 +0000

    Getting started with ze code.

---
+++ MODSRC/vazkii/tquarry/common/ThermalQuarry.java
@@ -0,0 +1,21 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the ThermalQuarry Mod.
+ * 
+ * ThermalQuarry is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * File Created @ [Dec 28, 2013, 6:27:38 PM (GMT)]
+ */
+package vazkii.tquarry.common;
+
+import vazkii.tquarry.common.lib.LibMisc;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = LibMisc.MOD_ID, name = LibMisc.MOD_NAME, version = LibMisc.MOD_VERSION, dependencies = LibMisc.DEPENDENCIES)
+@NetworkMod(clientSideRequired = true)
+public class ThermalQuarry {
+
+}
---
+++ MODSRC/vazkii/tquarry/common/lib/LibMisc.java
@@ -0,0 +1,20 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the ThermalQuarry Mod.
+ * 
+ * ThermalQuarry is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * File Created @ [Dec 28, 2013, 6:28:57 PM (GMT)]
+ */
+package vazkii.tquarry.common.lib;
+
+public final class LibMisc {
+
+	public static final String MOD_ID = "ThermalQuarry";
+	public static final String MOD_NAME = "Thermal Quarry";
+	public static final String MOD_VERSION = "ANT:VERSION-ANT:BUILD";
+	public static final String DEPENDENCIES = "required-after:ThermalExpansion";
+	
+}
